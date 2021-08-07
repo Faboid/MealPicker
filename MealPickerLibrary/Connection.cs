c680e87c3df04a662c60459b60f4e3ec9be1a690
Faboid <fabio.meloni.2000@tiscali.it>
2021-08-07 19:29:12 +0200

    Initial Commit.

---
+++ MealPickerLibrary/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPickerLibrary {
+    public static class Connection {
+
+        private static HttpClient client;
+        private static string apiKey;
+
+        static Connection() {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.spoonacular.com/recipes/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        static async Task<T> GetRecipeAsync<T>(string path) where T: class {
+            T product = null;
+            using(HttpResponseMessage response = await client.GetAsync(path)) {
+                if(response.IsSuccessStatusCode) {
+                    product = await response.Content.ReadAsAsync<T>();
+                }
+            }
+            return product;
+        }
+
+        static string BuildRequestRecipeByID(string ID) {
+            return $"{ID}/information?apiKey={apiKey}&includeNutrition=false";
+        }
+
+    }
+}
