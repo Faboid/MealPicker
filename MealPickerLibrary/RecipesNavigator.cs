aa1816ae90463a18e6f55ebe9b81fcee3704bdfc
Faboid <fabio.meloni.2000@tiscali.it>
2021-08-19 11:23:51 +0200

    Protected the "Next" function from calls during its execution.

--- MealPickerLibrary/RecipesNavigator.cs
+++ MealPickerLibrary/RecipesNavigator.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MealPickerLibrary {
@@ -13,18 +14,29 @@ namespace MealPickerLibrary {
         //todo - start a timer that deletes all cached recipes after an hour to follow spoonacular's TOS
         private static List<RecipeModel> recipes = new List<RecipeModel>();
 
+        private static SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
         public static async Task<bool> Next() {
+
+            if(!semaphore.Wait(50)) {
+                return false;
+            }
+
             if(CurrentRecipe is not null) {
                 recipes.Remove(CurrentRecipe);
                 CurrentRecipe = null;
             }
 
             if(recipes.Count is 0) {
+
                 //call API to refresh count
                 recipes = (await Connection.GetRandomRecipesAsync(150)).Recipes.ToList();
+
             }
 
             CurrentRecipe = recipes.First();
+
+            semaphore.Release();
 
             if(ValidateValues() is false) {
                 await Next();
