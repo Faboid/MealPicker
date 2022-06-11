be4072546cec7e66cf040057fd790448b63184fd
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-11 12:27:56 +0200

    Added models to core library.

---
+++ MealPicker.Core/Models/ListRecipesResult.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace MealPicker.Core.Models;
+
+public class ListRecipesResult {
+
+    [JsonProperty("recipes")]
+    public RecipeModel[] Recipes { get; set; }
+
+}
---
+++ MealPicker.Core/Models/RecipeModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MealPicker.Core.Models {
+    public class RecipeModel {
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+
+        //url to the image
+        public string Image { get; set; }
+
+        public int ReadyInMinutes { get; set; }
+        public int Servings { get; set; }
+
+        //authors name
+        public string SourceName { get; set; }
+        public string SourceUrl { get; set; }
+        public IngredientModel[] extendedIngredients { get; set; }
+        public string Summary { get; set; }
+
+    }
+}
