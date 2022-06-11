10b46cd37820176cedff09f229bb51649eaf1ca2
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-11 10:45:40 +0200

    Extracted regular expression to cache it properly.

--- MealPickerLibrary/Conversions/HTMLtoPlainText.cs
+++ MealPickerLibrary/Conversions/HTMLtoPlainText.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace MealPickerLibrary.Conversions {
     public static class HTMLtoPlainText {
+        
+        //regular expression by vfilby on https://stackoverflow.com/questions/286813/how-do-you-convert-html-to-plain-text
+        private readonly static Regex tags = new("<[^>]*>", RegexOptions.Compiled);
 
         public static string Convert(string HTML_Text) {
-            //regular expression by vfilby on https://stackoverflow.com/questions/286813/how-do-you-convert-html-to-plain-text
-            Regex tags = new Regex("<[^>]*>");
-
             return tags.Replace(HTML_Text, string.Empty);
         }
 
