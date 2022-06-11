b1fc80e92b8a34b03651746c596d04fca0e47a0c
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-11 19:19:59 +0200

    Implemented a class to represent the API key.

---
+++ MealPicker.Core/Services/API_Key.cs
@@ -0,0 +1,18 @@
+﻿namespace MealPicker.Core.Services; 
+
+public class API_Key {
+
+    private readonly string key;
+    private const string apiKeySign = "apiKey";
+
+    public API_Key(string key) {
+        this.key = key;
+    }
+
+    public override string ToString() => key;
+
+    public string GetQueryTestKey() => $"random?{apiKeySign}={key}&number=1&limitLicense=true";
+    public string GetQueryRandomRecipes(int recipes) => $"random?{apiKeySign}={key}&number={recipes}&limitLicense=true";
+    public string GetQueryRecipeById(string id) => $"{id}/information?{apiKeySign}={key}&includeNutrition=false";
+
+}
