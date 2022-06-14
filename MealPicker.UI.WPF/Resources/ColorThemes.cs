eecefa5382a524fa729068f64a9ed4fdf43875ac
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-14 15:22:15 +0200

    Fully implemented framework of dynamic color theme that can be changed at runtime.

--- MealPicker.UI.WPF/Resources/ColorThemes.cs
+++ MealPicker.UI.WPF/Resources/ColorThemes.cs
@@ -4,68 +4,103 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace MealPicker.UI.WPF.Resources {
-    public class ColorThemes : IColorTheme {
+    public partial class ColorThemes : ResourceDictionary, IColorThemePainter {
 
-        public static ColorThemes Singleton { get; private set; } = new(ColorTheme.Dark);
+        public static IColorThemePainter Painter => brushesDictionary.Value;
 
-        SolidColorBrush IColorTheme.DeepestBG => _theme.DeepestBG;
+        private static Lazy<ColorThemes> brushesDictionary { get; } = new Lazy<ColorThemes>(() => new(GetResourceDictionary));
 
-        SolidColorBrush IColorTheme.DeepBG => _theme.DeepBG;
+        private static ResourceDictionary GetResourceDictionary => Application
+            .Current
+            .Resources
+            .MergedDictionaries
+            .Where(x => x.Source.AbsoluteUri == "pack://application:,,,/Resources/ColorThemes.xaml")
+            .First();
 
-        SolidColorBrush IColorTheme.ForeBG => _theme.ForeBG;
+        public string DeepestBG { get; } = nameof(IColorTheme.DeepestBG);
+        public string DeepBG { get; } = nameof(IColorTheme.DeepBG);
+        public string ForeBG { get; } = nameof(IColorTheme.ForeBG);
+        public string LightText { get; } = nameof(IColorTheme.LightText);
 
-        SolidColorBrush IColorTheme.LightText => _theme.LightText;
+        public ColorThemes() {
+            InitializeComponent();
+            dict = this;
+            Apply(0);
+        }
 
-        private ColorThemes(ColorTheme theme) {
-            Change(theme);
+        private ColorThemes(ResourceDictionary dictionary) {
+            dict = dictionary;
+            Apply(0);
         }
 
         private IColorTheme _theme;
+        private ResourceDictionary dict;
 
         [MemberNotNull(nameof(_theme))]
-        public void Change(ColorTheme theme) {
+        public void Apply(ColorTheme theme) {
             _theme = theme switch {
                 ColorTheme.Dark => new DarkTheme(),
+                ColorTheme.Light => new LightTheme(),
                 _ => new DarkTheme(), //default
             };
 
             OnChangedTheme?.Invoke(this, _theme);
+
+            dict["LightText"] = new SolidColorBrush(_theme.LightText);
+            dict["DeepestBG"] = new SolidColorBrush(_theme.DeepestBG);
         }
 
         public event EventHandler<IColorTheme>? OnChangedTheme;
 
     }
 
+    public interface IColorThemePainter {
+        public void Apply(ColorTheme theme);
+    }
+
     public enum ColorTheme {
         Dark,
+        Light
     }
 
     public class DarkTheme : IColorTheme {
+        public Color DeepestBG => "#FF272121".ToColor();
+        public Color DeepBG => throw new NotImplementedException();
+        public Color ForeBG => throw new NotImplementedException();
+        public Color LightText => "FFFFFFFF".ToColor();
 
-        SolidColorBrush IColorTheme.DeepestBG => throw new NotImplementedException();
-        SolidColorBrush IColorTheme.DeepBG => throw new NotImplementedException();
-        SolidColorBrush IColorTheme.ForeBG => throw new NotImplementedException();
-        SolidColorBrush IColorTheme.LightText => throw new NotImplementedException();
+    }
+
+    public class LightTheme : IColorTheme {
+        public Color DeepestBG => "FFFFFFFF".ToColor();
+        public Color DeepBG => throw new NotImplementedException();
+        public Color ForeBG => throw new NotImplementedException();
+        public Color LightText => "FF272121".ToColor();
 
     }
 
     public interface IColorTheme {
 
         //temp names
-        SolidColorBrush DeepestBG { get; }
-        SolidColorBrush DeepBG { get; }
-        SolidColorBrush ForeBG { get; }
-        SolidColorBrush LightText { get; }
+        Color DeepestBG { get; }
+        Color DeepBG { get; }
+        Color ForeBG { get; }
+        Color LightText { get; }
 
     }
 
     internal static class Extensions {
 
         //todo - test this method
-        public static SolidColorBrush ToBrush(string hex) {
+        public static Color ToColor(this string hex) {
+
+            if(hex[0] == '#') {
+                hex = hex[1..];
+            }
 
             if(hex.Length == 6) {
                 var color = Color
@@ -74,7 +109,7 @@ namespace MealPicker.UI.WPF.Resources {
                         Convert.ToByte(hex.Substring(2, 2), 16),
                         Convert.ToByte(hex.Substring(4, 2), 16)
                     );
-                return new(color);
+                return color;
             }
 
             if(hex.Length == 8) {
@@ -85,7 +120,7 @@ namespace MealPicker.UI.WPF.Resources {
                         Convert.ToByte(hex.Substring(4, 2), 16),
                         Convert.ToByte(hex.Substring(6, 2), 16)
                     );
-                return new(color);
+                return color;
             }
 
             throw new ArgumentException($"The given hex ({hex}) is not valid.", nameof(hex));
