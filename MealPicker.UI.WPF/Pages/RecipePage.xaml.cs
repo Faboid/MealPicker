7432290a55474f7f8b2600fe71563fb67915ffbb
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-17 18:39:04 +0200

    Implemented usage of RecipeNavigator with page.

--- MealPicker.UI.WPF/Pages/RecipePage.xaml.cs
+++ MealPicker.UI.WPF/Pages/RecipePage.xaml.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MealPicker.Core;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace MealPicker.UI.WPF.Pages
-{
-    /// <summary>
-    /// Interaction logic for RecipePage.xaml
-    /// </summary>
-    public partial class RecipePage : Page
-    {
-        public RecipePage()
-        {
-            InitializeComponent();
-        }
+namespace MealPicker.UI.WPF.Pages; 
+
+/// <summary>
+/// Interaction logic for RecipePage.xaml
+/// </summary>
+public partial class RecipePage : Page {
+
+    private readonly RecipesNavigator navigator;
+
+    public RecipePage(RecipesNavigator navigator) {
+        InitializeComponent();
+        this.navigator = navigator;
+    }
+
+    private async void RandomButton_Click(object sender, RoutedEventArgs e) {
+        var option = await navigator.NextAsync();
+        RecipeView.Recipe = option.Match(
+            some => some,
+            () => RecipeView.Recipe
+        );
     }
 }
