21f45cbd9cc2d415ed476b7b939f13f04f99bb96
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-19 15:20:43 +0200

    Added interface to support different pages that have specific confirmation method.

---
+++ MealPicker.UI.WPF/Pages/Interface/IForm.cs
@@ -0,0 +1,11 @@
+﻿using MealPicker.Core.Services;
+using MealPicker.Utils;
+using System.Threading.Tasks;
+
+namespace MealPicker.UI.WPF.Pages.Interface {
+    internal interface IForm {
+
+        public Task<Option<ConnectionService>> ConfirmAsync();
+
+    }
+}
