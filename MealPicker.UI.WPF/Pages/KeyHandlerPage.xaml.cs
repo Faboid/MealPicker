629c60e4736371082447892f193089e5e604274b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-19 15:22:26 +0200

    Implemented confirmation method's good path. Will have to implement next: error messages and saving.

--- MealPicker.UI.WPF/Pages/KeyHandlerPage.xaml.cs
+++ MealPicker.UI.WPF/Pages/KeyHandlerPage.xaml.cs
@@ -1,20 +1,42 @@
-﻿using System.Windows;
+﻿using MealPicker.Core.Services;
+using MealPicker.UI.WPF.Pages.Interface;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 
-namespace MealPicker.UI.WPF.Pages
-{
+namespace MealPicker.UI.WPF.Pages {
     /// <summary>
     /// Interaction logic for KeyHandlerPage.xaml
     /// </summary>
-    public partial class KeyHandlerPage : Page
-    {
-        public KeyHandlerPage()
-        {
+    public partial class KeyHandlerPage : Page {
+
+        private readonly IForm current;
+        public event EventHandler<ConnectionService>? CloseAndReturn;
+
+        public KeyHandlerPage() {
             InitializeComponent();
+            
+            //todo - add a check on whether the api key exists
+            //NewKeyForm will be used then no apikey is saved
+            current = new NewKeyForm();
+            PageContainer.Navigate(current);
         }
 
-        private void ConfirmButton_Click(object sender, RoutedEventArgs e) {
+        private async void ConfirmButton_Click(object sender, RoutedEventArgs e) {
+            var result = await current.ConfirmAsync();
+            if(result.Result() != Utils.Options.OptionResult.Some) {
+                //do nothing if it fails. Might consider adding some sort of form-wide popup, but it's handled by the IForm currently.
+                return;
+            }
 
+            //since it's OptionResult.Some(or it would've returned early) the exception won't ever be hit.
+            //might do this differently in a future update
+            var conn = result.Match(
+                    some => some,
+                    () => throw new Exception()
+                );
+
+            CloseAndReturn?.Invoke(this, conn);
         }
     }
 }
---
+++ MealPicker.UI.WPF/Pages/NewKeyForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MealPicker.Core.Services;
+using MealPicker.UI.WPF.Pages.Interface;
+using MealPicker.Utils;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MealPicker.UI.WPF.Pages {
+    /// <summary>
+    /// Interaction logic for NewKeyForm.xaml
+    /// </summary>
+    public partial class NewKeyForm : Page, IForm {
+        public NewKeyForm() {
+            InitializeComponent();
+        }
+
+        public async Task<Option<ConnectionService>> ConfirmAsync() {
+
+            string password = PasswordTextBox.Text;
+
+            //todo - implement error message
+            if(password != ConfirmPasswordTextBox.Text) {
+                return Option.None<ConnectionService>();
+            }
+
+            API_Key key = new(APIKeyTextBox.Text);
+            var result = await ConnectionService.CreateConnectionAsync(key);
+
+            return result
+                .Match(
+                    some => SaveAndReturnConnection(key, password, some),
+                    error => DisplayErrors(error),
+                    () => Option.None<ConnectionService>()
+                );
+        }
+
+        private Option<ConnectionService> SaveAndReturnConnection(API_Key key, string password, ConnectionService connection) {
+
+            //todo - save the key to file
+
+            return connection;
+        }
+
+        private Option<ConnectionService> DisplayErrors(IConnectionService.FailResult failResult) {
+
+            var stuff = failResult.StatusCode switch {
+                HttpStatusCode.GatewayTimeout => "Failed to connect to the server.",
+                HttpStatusCode.Unauthorized => "The given key is not valid.",
+                _ => "The verification process has failed for an unknown reason. Please check everything is correct and retry again."
+            };
+
+            //todo - display the error
+
+            return Option.None<ConnectionService>();
+        }
+
+    }
+}
