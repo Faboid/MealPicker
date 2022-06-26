c6484f5c0f4e49a9f9a04f23ee18cde12116717e
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-26 11:59:23 +0200

    Implemented ThemeSelector, a combobox used to select the current theme.

---
+++ MealPicker.UI.WPF/UserControls/ThemeSelector.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MealPicker.UI.WPF.Resources;
+using System.Windows.Controls;
+
+namespace MealPicker.UI.WPF.UserControls {
+    /// <summary>
+    /// Interaction logic for ThemeSelector.xaml
+    /// </summary>
+    public partial class ThemeSelector : UserControl {
+        public ThemeSelector() {
+            InitializeComponent();
+        }
+
+        private void ThemesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if(ThemesComboBox.SelectedItem is ColorTheme theme) {
+                ColorThemes.Painter.Apply(theme);
+            } else {
+                //todo - handle unexpected result
+            }
+            ThemesComboBox.Text = ""; //clear text
+        }
+    }
+}
