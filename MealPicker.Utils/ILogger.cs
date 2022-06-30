b28ae58ff0bb284d28c1c2123f76c62388e0b6f8
Faboid <72757782+Faboid@users.noreply.github.com>
2022-06-30 20:20:44 +0200

    (untested) Introduced ILogger and Logger.

---
+++ MealPicker.Utils/ILogger.cs
@@ -0,0 +1,33 @@
+﻿namespace MealPicker.Utils; 
+
+public interface ILogger {
+
+    public enum Level {
+        Debug,
+        Info,
+        Warning,
+        Error,
+    }
+
+    /// <summary>
+    /// Logs the given message to a file, using the given <see cref="ILogger.Level"/>.
+    /// </summary>
+    /// <param name="level"></param>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    Task LogAsync(Level level, string message);
+    Task LogErrorAsync(string message);
+    Task LogWarningAsync(string message);
+    Task LogInfoAsync(string message);
+
+    /// <summary>
+    /// Logs the given message to a file, using the given <see cref="ILogger.Level"/>.
+    /// </summary>
+    /// <param name="level"></param>
+    /// <param name="message"></param>
+    void Log(Level level, string message);
+    void LogError(string message);
+    void LogWarning(string message);
+    void LogInfo(string message);
+
+}
