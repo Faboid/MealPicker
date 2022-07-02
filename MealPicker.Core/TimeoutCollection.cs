994b02ecf519bf19af1bb929bae9bbae6730075d
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-02 20:20:56 +0200

    Implemented documentation for the public methods.

--- MealPicker.Core/TimeoutCollection.cs
+++ MealPicker.Core/TimeoutCollection.cs
@@ -3,8 +3,18 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace MealPicker.Core; 
 
+/// <summary>
+/// A list that will delete all its contents after a given time.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 internal class TimeoutCollection<T> : IDisposable {
 
+    /// <summary>
+    /// Creates an instance of <see cref="TimeoutCollection{T}"/>.
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="recipes"></param>
+    /// <param name="timeout"></param>
     public TimeoutCollection(ILogger logger, IList<T> recipes, TimeSpan timeout) {
         this.timeout = timeout;
         this.logger = logger;
@@ -23,6 +33,10 @@ internal class TimeoutCollection<T> : IDisposable {
         logger.LogInfo("The TimeoutCollection's timer has expired and the recipes have been cleared.");
     }
 
+    /// <summary>
+    /// Clears all remaining values, restarts the timer, and stores the given <paramref name="recipes"/>.
+    /// </summary>
+    /// <param name="recipes"></param>
     [MemberNotNull(nameof(timer), nameof(recipes))]
     public void Renew(IList<T> recipes) {
         this.recipes?.Clear();
@@ -32,6 +46,10 @@ internal class TimeoutCollection<T> : IDisposable {
         this.recipes = recipes;
     }
 
+    /// <summary>
+    /// Returns the next recipe, then deletes it from this collection.
+    /// </summary>
+    /// <returns>An option with <typeparamref name="T"/> if there is at least one value; otherwise, <see cref="Option.None{TValue}"/>.</returns>
     public Option<T> Next() {
 
         var recipe = recipes.FirstOrDefault();
