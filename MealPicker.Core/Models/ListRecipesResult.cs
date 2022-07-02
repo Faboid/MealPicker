d5659c6e71abe227a2bbaabe26a3cf91db827c24
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-02 20:04:50 +0200

    Provided documentation for the recipe models.

--- MealPicker.Core/Models/ListRecipesResult.cs
+++ MealPicker.Core/Models/ListRecipesResult.cs
@@ -1,7 +1,13 @@
 ﻿namespace MealPicker.Core.Models;
 
+/// <summary>
+/// A wrapper for an array of <see cref="RecipeModel"/>.
+/// </summary>
 public class ListRecipesResult {
 
+    /// <summary>
+    /// A list of <see cref="RecipeModel"/>.
+    /// </summary>
     public RecipeModel[] Recipes { get; set; }
 
 }
--- MealPicker.Core/Models/RecipeModel.cs
+++ MealPicker.Core/Models/RecipeModel.cs
@@ -2,23 +2,49 @@
 
 public class RecipeModel {
 
+    /// <summary>
+    /// The ID of the recipe.
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// The title of the recipe.
+    /// </summary>
     public string Title { get; set; }
 
+    /// <summary>
+    /// The URL to get the image.
+    /// </summary>
     public string Image { get; set; }
 
+    /// <summary>
+    /// Total minutes required to complete the recipe.
+    /// </summary>
     public int ReadyInMinutes { get; set; }
 
+    /// <summary>
+    /// Number of servings.
+    /// </summary>
     public int Servings { get; set; }
 
-    //authors name
+    /// <summary>
+    /// Author's name.
+    /// </summary>
     public string SourceName { get; set; }
 
+    /// <summary>
+    /// URL to the recipe.
+    /// </summary>
     public string SourceUrl { get; set; }
 
+    /// <summary>
+    /// A list with all ingredients.
+    /// </summary>
     public IngredientModel[] ExtendedIngredients { get; set; }
 
+    /// <summary>
+    /// The summary of the recipe.
+    /// </summary>
     public string Summary { get; set; }
 
 }
