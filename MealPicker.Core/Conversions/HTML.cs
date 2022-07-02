d2ea9fc25c7c3ee279362e0d734ba9a614bc4ffb
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-02 19:53:30 +0200

    Provided documentation for static HTML method.

--- MealPicker.Core/Conversions/HTML.cs
+++ MealPicker.Core/Conversions/HTML.cs
@@ -2,11 +2,19 @@
 
 namespace MealPicker.Core.Conversions; 
 
+/// <summary>
+/// Provides HTML-related static methods.
+/// </summary>
 public static class HTML {
 
     //regular expression by vfilby on https://stackoverflow.com/questions/286813/how-do-you-convert-html-to-plain-text
     private readonly static Regex tags = new("<[^>]*>", RegexOptions.Compiled);
 
+    /// <summary>
+    /// Erases all HTML markup and returns a plain text string.
+    /// </summary>
+    /// <param name="HTML_Text">The string to clean from HTML markup.</param>
+    /// <returns>A plain text string.</returns>
     public static string ToPlainText(string HTML_Text) {
         return tags.Replace(HTML_Text, string.Empty);
     }
