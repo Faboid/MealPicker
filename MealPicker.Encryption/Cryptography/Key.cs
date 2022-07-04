ed415f533035670e0ad628d2c8ae9b8464cb5021
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-04 11:59:25 +0200

    Implemented documentation for Encryption.Cryptography

--- MealPicker.Encryption/Cryptography/Key.cs
+++ MealPicker.Encryption/Cryptography/Key.cs
@@ -2,14 +2,26 @@
 using System.Text;
 
 namespace MealPicker.Encryption.Cryptography {
+
+    /// <summary>
+    /// Represents a password key.
+    /// </summary>
     public class Key : IDisposable {
 
+        /// <summary>
+        /// Creates an instance of <see cref="Key"/> using the given byte array. The byte array will be copied and then set to 0.
+        /// </summary>
+        /// <param name="password"></param>
         public Key(byte[] password) : this(Encoding.Unicode.GetChars(password)) {
             for(int i = 0; i < password.Length; i++) {
                 password[i] = 0;
             }
         }
 
+        /// <summary>
+        /// Creates an instance of <see cref="Key"/> using the given char array. The chair array will be copied and then set to 0.
+        /// </summary>
+        /// <param name="password"></param>
         public Key(char[] password) {
             key = new char[password.Length];
             gcHandle = GCHandle.Alloc(key, GCHandleType.Pinned);
@@ -20,6 +32,10 @@ namespace MealPicker.Encryption.Cryptography {
         private readonly char[] key;
         private GCHandle gcHandle;
 
+        /// <summary>
+        /// Gets the key. To ensure proper cleanup, it's suggested to not change/copy any element.
+        /// </summary>
+        /// <returns></returns>
         public char[] Get() {
             return key;
         }
--- MealPicker.Encryption/Cryptography/SaltGenerator.cs
+++ MealPicker.Encryption/Cryptography/SaltGenerator.cs
@@ -2,19 +2,38 @@
 using System.Security.Cryptography;
 
 namespace MealPicker.Encryption.Cryptography {
+
+    /// <summary>
+    /// Handles generating salt.
+    /// </summary>
     internal class SaltGenerator {
 
         readonly private int length;
         readonly private int minLength = 32;
         readonly private int maxLength = 64;
 
+        /// <summary>
+        /// Creates and instance of <see cref="SaltGenerator"/> based on the given <paramref name="password"/>.
+        /// </summary>
+        /// <param name="password"></param>
         public SaltGenerator(char[] password) {
             int passValue = password.Sum(x => x / (password.Length / 2));
             length = new SaltRandom(minLength, maxLength, passValue).Next();
         }
 
+        /// <summary>
+        /// Generates a random salt. The length is dependent on the password given on initialization.
+        /// </summary>
+        /// <returns></returns>
         public byte[] CreateSalt() => RandomNumberGenerator.GetBytes(length);
 
+        /// <summary>
+        /// Extracts the salt from an encrypted string. 
+        /// Due to the variable-length nature of the pre-generated salt, 
+        /// it's necessary to use the same password used previously for encryption.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public byte[] ExtractSalt(ref string text) {
             byte[] bytes = Convert.FromBase64String(text);
             byte[] salt = bytes.Take(length).ToArray();
