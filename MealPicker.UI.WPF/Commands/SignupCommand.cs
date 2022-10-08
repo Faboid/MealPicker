00f42e69366c43068e2000adb1d3bfc76033fcd8
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-08 11:52:31 +0200

    Implemented sign up view.

---
+++ MealPicker.UI.WPF/Commands/SignupCommand.cs
@@ -0,0 +1,7 @@
+using System.Threading.Tasks;
+
+namespace MealPicker.UI.WPF.Commands;
+
+public class SignupCommand : AsyncCommandBase {
+    protected override Task ExecuteAsync(object? parameter) => throw new System.NotImplementedException();
+}
--- MealPicker.UI.WPF/ViewModels/SignupViewModel.cs
+++ MealPicker.UI.WPF/ViewModels/SignupViewModel.cs
@@ -1,7 +1,32 @@
+using MealPicker.UI.WPF.Commands;
+using System.Windows.Input;
+
 namespace MealPicker.UI.WPF.ViewModels;
 
 public class SignupViewModel : ViewModelBase {
 
+	private string _apyKey = "";
+	public string ApiKey {
+		get { return _apyKey; }
+		set { SetAndRaise(nameof(ApiKey), ref _apyKey, value); }
+	}
 
+	private string _password = "";
+	public string Password {
+		get { return _password; }
+		set { SetAndRaise(nameof(Password), ref _password, value); }
+	}
+
+	private string _repeatPassword = "";
+	public string RepeatPassword {
+		get { return _repeatPassword; }
+		set { SetAndRaise(nameof(RepeatPassword), ref _repeatPassword, value); }
+	}
+
+	public ICommand SignupCommand { get; }
+
+	public SignupViewModel() {
+		SignupCommand = new SignupCommand();
+	}
 
 }
