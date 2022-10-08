0ccda311e730b45514062cbadec17c96df3aec85
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-08 10:57:01 +0200

    Implemented usercontrols to reduce boilerplate on creating new forms.

---
+++ MealPicker.UI.WPF/Components/FormItem.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MealPicker.UI.WPF.Components;
+/// <summary>
+/// Interaction logic for FormItem.xaml
+/// </summary>
+public partial class FormItem : UserControl {
+
+    public string Text {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(FormItem), new PropertyMetadata(string.Empty));
+
+    public string Label {
+        get { return (string)GetValue(LabelProperty); }
+        set { SetValue(LabelProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty LabelProperty =
+        DependencyProperty.Register("Label", typeof(string), typeof(FormItem), new PropertyMetadata(string.Empty));
+
+    public FormItem() {
+        InitializeComponent();
+    }
+}
---
+++ MealPicker.UI.WPF/Components/FormWrapper.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MealPicker.UI.WPF.Components;
+/// <summary>
+/// Interaction logic for FormWrapper.xaml
+/// </summary>
+[ContentProperty("Items")]
+public partial class FormWrapper : UserControl {
+
+    public ICommand ComfirmFormCommand {
+        get { return (ICommand)GetValue(ComfirmFormCommandProperty); }
+        set { SetValue(ComfirmFormCommandProperty, value); }
+    }
+
+    public static readonly DependencyProperty ComfirmFormCommandProperty =
+        DependencyProperty.Register("ComfirmFormCommand", typeof(ICommand), typeof(FormWrapper), new PropertyMetadata(null));
+
+    public IEnumerable<FormItem> FormItems {
+        get { return (IEnumerable<FormItem>)GetValue(FormItemsProperty); }
+        set { SetValue(FormItemsProperty, value); }
+    }
+
+    public static readonly DependencyProperty FormItemsProperty = ItemsControl.ItemsSourceProperty.AddOwner(typeof(FormWrapper), new PropertyMetadata(null));
+
+    public ItemCollection Items {
+        get => _itemsControl.Items;
+    }
+
+    public FormWrapper() {
+        InitializeComponent();
+    }
+
+}
