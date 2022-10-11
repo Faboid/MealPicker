9aea89f620e98a7103aeb095c2d539de716a68ed
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-11 15:15:20 +0200

    Implemented a loaded command pipeline to load the elements automatically as soon as the RecipeGeneratorViewModel is loaded.

--- MealPicker.UI.WPF/ViewModels/RecipeGeneratorViewModel.cs
+++ MealPicker.UI.WPF/ViewModels/RecipeGeneratorViewModel.cs
@@ -2,6 +2,7 @@ using MealPicker.UI.WPF.Commands;
 using MealPicker.UI.WPF.Services;
 using MealPicker.UI.WPF.Stores;
 using Microsoft.Extensions.Logging;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MealPicker.UI.WPF.ViewModels;
@@ -10,9 +11,11 @@ public class RecipeGeneratorViewModel : ViewModelBase {
 
 	public RecipeViewModel RecipeViewModel { get; } = new();
 	public ICommand GenerateRandomRecipeCommand { get; }
+	public ICommand LoadedCommand { get; }
 
 	public RecipeGeneratorViewModel(RecipeStore recipeStore, INotificationService notificationService, ILoggerFactory? loggerFactory = null) {
 		GenerateRandomRecipeCommand = new GetNextRandomRecipeCommand(this, recipeStore, notificationService, loggerFactory);
+		LoadedCommand = new RelayCommand(() => GenerateRandomRecipeCommand.Execute(null));
 	}
 
 }
--- MealPicker.UI.WPF/Views/RecipeGeneratorView.xaml.cs
+++ MealPicker.UI.WPF/Views/RecipeGeneratorView.xaml.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MealPicker.UI.WPF.Views;
 /// <summary>
 /// Interaction logic for RecipeGeneratorView.xaml
 /// </summary>
 public partial class RecipeGeneratorView : UserControl {
+    
     public RecipeGeneratorView() {
         InitializeComponent();
+    }
+
+    public ICommand LoadedCommand {
+        get { return (ICommand)GetValue(LoadedCommandProperty); }
+        set { SetValue(LoadedCommandProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for LoadedCommand.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty LoadedCommandProperty =
+        DependencyProperty.Register("LoadedCommand", typeof(ICommand), typeof(RecipeGeneratorView), new PropertyMetadata(null));
+
+    private void UserControl_Loaded(object sender, RoutedEventArgs e) {
+        LoadedCommand?.Execute(null);
     }
 }
